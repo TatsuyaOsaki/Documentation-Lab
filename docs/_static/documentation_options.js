c89b67056fab626dac58d3d48bee99ba60165365
Tatsuya Osaki <61047905+TatsuyaOsaki@users.noreply.github.com>
2023-07-13 01:43:04 -0400

    ds

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.01',
+    VERSION: '2.02',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
