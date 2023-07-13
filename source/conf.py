c527862303e57bf0c4a53d2854e58cc9dd9df55b
Tatsuya Osaki <61047905+TatsuyaOsaki@users.noreply.github.com>
2023-07-13 00:25:38 -0400

    d

--- source/conf.py
+++ source/conf.py
@@ -54,3 +54,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 html_static_path = []
+# html_static_path = ['_static']
+
+
+html_logo = "logo_documentation"
