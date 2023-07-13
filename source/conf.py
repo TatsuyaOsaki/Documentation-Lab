21987304c062f353d658153503a870248529489d
Tatsuya Osaki <61047905+TatsuyaOsaki@users.noreply.github.com>
2023-07-13 00:04:53 -0400

    ds

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Tatsuya Osaki'
 author = 'Tatsuya Osaki'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '2.0'
 
 
 # -- General configuration ---------------------------------------------------
