5ddaaa72b71724314a7184264846a9e5f224e34c
Tatsuya Osaki <osaki@mit.edu>
2023-07-22 18:00:07 +0100

    dss

--- source/conf.py
+++ source/conf.py
@@ -70,11 +70,8 @@ html_theme_options = {
     "show_toc_level": 1,
     "icon_links": [
         {
-            # Label for this link
             "name": "GitHub",
-            # URL where the link will redirect
-            "url": "https://github.com/<your-org>/<your-repo>",  # required
-            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "url": "https://github.com/TatsuyaOsaki/Documentation-Lab",  # required
             "icon": "fa-brands fa-square-github",
             # The type of image to be used (see below for details)
             "type": "fontawesome",
