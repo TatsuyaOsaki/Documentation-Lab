7dd2e7b119f4bd7f8e1bcbcda2be969e44213585
Tatsuya Osaki <osaki@mit.edu>
2023-07-24 23:13:07 +0100

    fd

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Tatsuya Osaki'
 author = 'Tatsuya Osaki'
 
 # The full version, including alpha/beta/rc tags
-release = '2.14'
+release = '2.15'
 
 
 # -- General configuration ---------------------------------------------------
