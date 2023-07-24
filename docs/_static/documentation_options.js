79758c38159743e9ee26b53265225d19c42cfdac
Tatsuya Osaki <osaki@mit.edu>
2023-07-24 20:30:45 +0100

    zl

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.1２',
+    VERSION: '2.13',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Tatsuya Osaki'
 author = 'Tatsuya Osaki'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1２'
+release = '2.13'
 
 
 # -- General configuration ---------------------------------------------------
